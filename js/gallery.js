41e4c6890581a7a9a55b76507a18acd28d2f81a5
alona007 <alona.olyksashenko@gmail.com>
2024-04-19 09:35:00 +0200

    2

--- js/gallery.js
+++ js/gallery.js
@@ -65,38 +65,22 @@ const images = [
   
 ];
 
-const galleryEl = document.querySelector("ul.gallery");
-
-const galleryMarkup = images.map((image) => {
+const galleryMarkup = images.map(({ preview, original, description }) => {
   const listItem = document.createElement("li");
   listItem.classList.add("gallery-item");
 
   const link = document.createElement("a");
   link.classList.add("gallery-link");
-  link.href = image.original;
+  link.href = original;
 
   const img = document.createElement("img");
   img.classList.add("gallery-image");
-  img.src = image.preview;
-  img.dataset.source = image.original;
-  img.alt = image.description;
+  img.src = preview;
+  img.dataset.source = original;
+  img.alt = description;
 
   link.appendChild(img);
   listItem.appendChild(link);
 
   return listItem;
-});
-
-galleryMarkup.forEach((item) => {
-  galleryEl.appendChild(item);
-});
-
-galleryEl.addEventListener("click", (event) => {
-  event.preventDefault();
-  
-   if (event.target.classList.contains("gallery-image")) {
-    const bigImgUrl = event.target.dataset.source;
-
-    basicLightbox.create(`<img width="1400" height="900" src="${bigImgUrl}">`).show();
-  }
 });
